360b91e8dead529a5dac6212794ede1232e08e95
Jinhoon Park <g22203@naver.com>
2023-06-11 19:30:57 +0900

    feat(programmers): 추가 자동차 평균 대여 기간 구하기

---
+++ src/main/programmers/sql/CalculatingCarAverageDuration.sql
@@ -0,0 +1,6 @@
+-- 자동차 평균 대여 기간 구하기
+SELECT car_id, round(avg(datediff(end_date, start_date) + 1),1) as average_duration
+from car_rental_company_rental_history
+group by car_id
+having average_duration >= 7
+order by 2 desc, 1 desc;
